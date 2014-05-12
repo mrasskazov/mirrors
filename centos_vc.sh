afa455972b71039c8abc23e51bb1e17b91854e45
Max Rasskazov <mrasskazov@mirantis.com>
2014-05-12 18:19:43 +0400

    Added centos mirroring w/saving previos mirror state

---
+++ centos_vc.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -x
+
+fatal() {
+  echo "$@"
+  /bin/rm -f /tmp/${SRC_OS}_updates
+  exit 1
+}
+
+export SRC_OS=centos
+export SRC="rsync://mirrors.msk.mirantis.net/mirrors/${SRC_OS}/"
+
+#export EXCLUDE="--exclude \"local*\" --exclude \"isos\""
+
+export DATE=$(date "+%Y-%m-%d-%H%M%S")
+
+export DST=/media/mirrors/mirrors
+export DST_TMP=$DST/files/$SRC_OS-processing-$DATE
+export DST_DIR=$DST/files/$SRC_OS-$DATE
+
+if [ -f /tmp/${SRC_OS}_updates ]; then
+    echo "Updates via rsync already running."
+    exit 0
+fi
+
+touch /tmp/${SRC_OS}_updates
+
+(rsync --verbose \
+      --quiet \
+      --archive \
+      --progress \
+      --delete \
+      --numeric-ids \
+      --acls \
+      --xattrs \
+      --link-dest=../../$SRC_OS \
+      --sparse \
+      $EXCLUDE \
+      $SRC \
+      $DST_TMP \
+&& cd $DST \
+&& mv $DST_TMP $DST_DIR \
+&& rm -f $DST/$SRC_OS \
+&& ln -s $DST_DIR $DST/$SRC_OS \
+&& /bin/rm -f /tmp/${SRC_OS}_updates) \
+|| \
+fatal "rsync failed"
+
