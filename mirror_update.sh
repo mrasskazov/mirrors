fb4954d78fba7433645f5ddf70d0516256d59556
Max Rasskazov <mrasskazov@mirantis.com>
2014-08-25 16:04:37 +0400

    Implemented creating of Jenkins mirror
    Change-Id: Ied9ec304a57477ee560338a60f0c633f379b3d0c

--- mirror_update.sh
+++ mirror_update.sh
@@ -22,14 +22,40 @@ case "$SRC_MIRR" in
         export EXCLUDE='--exclude .temp --exclude .lastsync --exclude .mirror.yandex.ru'
         ;;
     "jenkins")
-        SYNCTYPE=wget
-        export SRC="http://pkg.jenkins-ci.org/debian-stable/
-http://pkg.jenkins-ci.org/debian-stable/binary/Packages
-http://pkg.jenkins-ci.org/debian-stable/binary/Release
-http://pkg.jenkins-ci.org/debian-stable/binary/Release.gpg
-http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key"
+        export SRC="rsync://mirror.yandex.ru/mirrors/jenkins/debian-stable"
         function past_download() {
-            mv $LATEST/debian/jenkins-ci.org.key $LATEST/debian/Release.key
+            # Based on the method described here:
+            # http://troubleshootingrange.blogspot.com/2012/09/hosting-simple-apt-repository-on-centos.html
+
+            pushd $LATEST/debian-stable
+                dpkg-scanpackages -m . > Packages
+                gzip -9c Packages > Packages.gz
+
+                # Generate release file
+                cat > Release <<ENDRELEASE
+Architectures: all
+Date: $(date -Ru)
+Origin: jenkins-ci.org
+Suite: debian-stable
+ENDRELEASE
+
+                # Generate hashes
+                c1=(MD5Sum: SHA1: SHA256: SHA512:)
+                c2=(md5 sha1 sha256 sha512)
+
+                i=0
+                while [ $i -lt ${#c1[*]} ]; do
+                    echo ${c1[i]} >> Release
+                    for hashme in `find . -type f \( -name "Package*" -o -name "Release*" \)`; do
+                        chash=`openssl dgst -${c2[$i]} ${hashme}|cut -d" " -f 2`
+                        size=`stat -c %s ${hashme}`
+                        echo " ${chash} ${size} $(basename ${hashme})" >> Release
+                    done
+                    i=$(( $i + 1));
+                done
+                gpg --yes --armor -o Release.gpg -sb Release
+
+            popd
         }
         ;;
     *)
