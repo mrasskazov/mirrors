f0868b8f34d24d57e7f3a0435528eb07ae396c86
Max Rasskazov <mrasskazov@mirantis.com>
2014-09-04 18:10:30 +0400

    Added ${mirror}.yaml.txt file with current mirror stable version
    Change-Id: Iedc2f5df39131c76f230c5ef3349cdf84f7152b0

--- fuel_master_mirror_set_stable.sh
+++ fuel_master_mirror_set_stable.sh
@@ -17,6 +17,10 @@ FILESROOT=fwm/files
 
 mirrors_fail=""
 
+echo "mirror:
+    name: ${MIRROR}
+    stable: ${STABLE_VERSION}" > ${MIRROR}.yaml.txt
+
 for RSYNCHOST in $RSYNCHOSTS; do
     STABLE_EXISTS="$(rsync $RSYNCHOST::$RSYNCUSER/$FILESROOT/ \
         | awk '/^d.* '$STABLE_VERSION'$/ {print $NF}')"
@@ -27,6 +31,7 @@ for RSYNCHOST in $RSYNCHOSTS; do
             $(get_empty_dir)/ $RSYNCHOST::$RSYNCUSER/$RSYNCROOT/ \
             && rsync -vl $SYMLINK_FILE $RSYNCHOST::$RSYNCUSER/$RSYNCROOT/$MIRROR \
             && rsync -vl $STABLE_SYMLINK_FILE $RSYNCHOST::$RSYNCUSER/$FILESROOT/${STABLE_VERSION}-stable \
+            && rsync -v ${MIRROR}.yaml.txt $RSYNCHOST::$RSYNCUSER/$RSYNCROOT/ \
             || mirrors_fail+=" $RSYNCHOST"
     else
         mirrors_fail+=" $RSYNCHOST"
